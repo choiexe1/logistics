4a6f4b29ddbf4d9bc6de2d1265495a59e285e227
choiexe1 <choiexe1@gmail.com>
2024-12-19 00:30:42 +0900

    init

---
+++ src/main/resources/static/tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["../templates/**/*.html"],
+    theme: {
+        colors: {
+            'blue': '#47B7F8',
+            'purple': '#7e5bef',
+            'pink': '#ff49db',
+            'orange': '#f35815',
+            'gray': '#818181',
+            'gray-light': '#a1a1a1',
+            'black': '#111111',
+            'white': '#fafafa',
+            'red': '#ff2800'
+        },
+        extend: {},
+    },
+    plugins: [],
+}
+
