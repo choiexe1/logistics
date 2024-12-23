1829f4fc5c0f9ca126f7c06f73cae1ebb609bc26
choiexe1 <choiexe1@gmail.com>
2024-12-23 21:58:53 +0900

    창고 수정기능 Ajax -> Form 으로 변경

--- src/main/java/blog/devjay/logistics/dto/warehouse/CreateWarehouseDTO.java
+++ src/main/java/blog/devjay/logistics/dto/warehouse/CreateWarehouseDTO.java
@@ -4,7 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class UpdateWarehouseDTO {
+public class CreateWarehouseDTO {
     @NotBlank
     private String name;
     @NotBlank
--- src/main/java/blog/devjay/logistics/web/controller/WarehouseController.java
+++ src/main/java/blog/devjay/logistics/web/controller/WarehouseController.java
@@ -2,22 +2,23 @@ package blog.devjay.logistics.web.controller;
 
 import blog.devjay.logistics.domain.exception.NotFoundException;
 import blog.devjay.logistics.domain.warehouse.Warehouse;
+import blog.devjay.logistics.dto.warehouse.CreateWarehouseDTO;
 import blog.devjay.logistics.dto.warehouse.SearchWarehouseDTO;
 import blog.devjay.logistics.dto.warehouse.UpdateWarehouseDTO;
 import blog.devjay.logistics.service.WarehouseService;
 import blog.devjay.logistics.web.utils.PaginationUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/warehouse")
@@ -41,22 +42,62 @@ public class WarehouseController {
     }
 
     @GetMapping("/{warehouseId}")
-    public String warehouseView(Model model, @PathVariable("warehouseId") Long warehouseId) {
+    public String warehouseInfoView(Model model, @PathVariable("warehouseId") Long warehouseId) {
 
         try {
             Warehouse warehouse = warehouseService.findById(warehouseId);
-            model.addAttribute("warehouse", warehouse);
+            UpdateWarehouseDTO dto = new UpdateWarehouseDTO();
+            dto.setName(warehouse.getName());
+            dto.setLocation(warehouse.getLocation());
 
+            model.addAttribute("warehouse", warehouse);
+            model.addAttribute("updateWarehouseDTO", dto);
             return "views/warehouse/info";
         } catch (NotFoundException e) {
-            return "/warehouse";
+            return "redirect:/warehouse";
         }
     }
 
-    @PutMapping("/{warehouseId}")
-    @ResponseBody
-    public void updateWarehouse(@PathVariable("warehouseId") Long warehouseId, @RequestBody
-    UpdateWarehouseDTO dto) {
-        warehouseService.update(warehouseId, dto);
+    @PostMapping("/update/{warehouseId}")
+    public String updateWarehouse(@PathVariable("warehouseId") Long warehouseId,
+                                  @Validated @ModelAttribute("updateWarehouseDTO") UpdateWarehouseDTO dto,
+                                  BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error("error = {}", bindingResult.getAllErrors());
+            return "views/warehouse/info";
+        }
+
+        try {
+            warehouseService.update(warehouseId, dto);
+        } catch (Exception e) {
+            log.error("e", e);
+            return "views/warehouse/info";
+        }
+
+        return "redirect:/warehouse/{warehouseId}";
+    }
+
+    @GetMapping("/create")
+    public String createView(@ModelAttribute("warehouse") CreateWarehouseDTO dto) {
+        return "views/warehouse/create";
+    }
+
+    @PostMapping("/create")
+    public String create(@Validated @ModelAttribute("warehouse") CreateWarehouseDTO dto, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            log.error("error = {}", bindingResult.getAllErrors());
+            return "views/warehouse/create";
+        }
+
+        Warehouse warehouse = new Warehouse(dto.getName(), dto.getLocation());
+
+        try {
+            warehouseService.create(warehouse);
+        } catch (DuplicateKeyException e) {
+            bindingResult.rejectValue("name", "warehouse.name.exist");
+            return "views/warehouse/create";
+        }
+
+        return "redirect:/warehouse";
     }
 }
