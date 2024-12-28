5bdf0d4657f989fe255355f35047bdd93d25670a
choiexe1 <choiexe1@gmail.com>
2024-12-29 04:27:48 +0900

    제네릭 타입 제한

--- src/main/java/blog/devjay/logistics/repository/IRepository.java
+++ src/main/java/blog/devjay/logistics/repository/IRepository.java
@@ -1,20 +1,22 @@
 package blog.devjay.logistics.repository;
 
+import blog.devjay.logistics.dto.SearchDTO;
+import blog.devjay.logistics.dto.UpdateDTO;
 import java.util.List;
 import java.util.Optional;
 
-public interface IRepository<T, IDType, SearchDTO, UpdateDTO> {
+public interface IRepository<T, IDType, SearchType extends SearchDTO, UpdateType extends UpdateDTO> {
     IDType save(T t);
 
     Optional<T> findById(IDType id);
 
     List<T> findAll();
 
-    List<T> findAll(SearchDTO searchDTO);
+    List<T> findAll(SearchType searchType);
 
-    int findAllCount(SearchDTO searchDTO);
+    int findAllCount(SearchType searchType);
 
-    void update(IDType id, UpdateDTO updateDTO);
+    void update(IDType id, UpdateType updateType);
 
     void delete(IDType id);
 }
