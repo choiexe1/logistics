776cd8a8b38d79445055ff66f15d229bdcd5dc75
choiexe1 <choiexe1@gmail.com>
2025-01-10 16:04:28 +0900

    run.sh 수정

--- run.sh
+++ run.sh
@@ -13,9 +13,9 @@ fi
 
 nohup java -jar $JAR_PATH > $LOG_FILE 2>&1 &
 
-PID=$!
-echo $PID > $PID_FILE
-echo "Spring Boot 애플리케이션이 PID: $PID로 시작되었습니다."
+echo $! > $PID_FILE
+echo "Spring Boot 애플리케이션이 PID: $!로 시작되었습니다."
 
+echo "PID는 다음 파일에 기록됩니다: $PID_FILE"
 echo "로그는 다음 파일에 기록됩니다: $LOG_FILE"
 echo "로그를 확인하려면 다음 명령어를 사용하세요: tail -f $LOG_FILE"
